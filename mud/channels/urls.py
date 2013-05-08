fd09d01dc4e7bdf4343c763009c8f698fef0798e
Nikolay Khokhlov <kolya.khokhlov@gmail.com>
2013-05-08 13:19:08 +0400

    Refactoring channels view

--- mud/channels/urls.py
+++ mud/channels/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(ur'^offtop/', views.offtop, name='offtop'),
     url(ur'^bolt/', views.bolt, name='bolt'),
     url(ur'^scream/', views.scream, name='scream'),
+    url(ur'^all/', views.all, name='all'),
 )
--- mud/channels/views.py
+++ mud/channels/views.py
@@ -1,12 +1,12 @@
+# coding=utf-8
+
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404
 
 from channels.models import *
 
-def channel(request, type, template):
-    o = Channel.objects.filter(type__exact = type)
-    
+def channel(request, o, title):
     paginator = Paginator(o.order_by('date').reverse(), 50)
     page = request.GET.get('page')
     try:
@@ -16,15 +16,18 @@ def channel(request, type, template):
     except EmptyPage:
         o = paginator.page(paginator.num_pages)
 
-    context = {'messages': o, }
-    return render(request, template, context)
+    context = {'messages': o, 'title': title}
+    return render(request, 'channels/index.html', context)
 
 
 def offtop(request):
-    return channel(request, Channel.OFFTOP, 'channels/offtop.html')
+    return channel(request, Channel.objects.filter(type__exact = Channel.OFFTOP), u'Оффтоп')
 
 def bolt(request):
-    return channel(request, Channel.BOLT, 'channels/bolt.html')
+    return channel(request, Channel.objects.filter(type__exact = Channel.BOLT), u'Болтать')
 
 def scream(request):
-    return channel(request, Channel.SCREAM, 'channels/scream.html')
+    return channel(request, Channel.objects.filter(type__exact = Channel.SCREAM), u'Орать')
+
+def all(request):
+    return channel(request, Channel.objects.all(), u'Все')
