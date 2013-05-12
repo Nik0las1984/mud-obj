94b608e043ee44a09f83bf7b5339a38d379cfee6
Nikolay Khokhlov <kolya.khokhlov@gmail.com>
2013-05-12 13:48:50 +0400

    Addef filtration by parameters

--- mud/objects/urls.py
+++ mud/objects/urls.py
@@ -5,5 +5,6 @@ from objects import views
 urlpatterns = patterns('',
     url(ur'^obj/(?P<name>[\w\s-]+)/$', views.obj, name='obj'),
     url(r'^shop/', views.shop, name='shop'),
+    url(r'^params/', views.params, name='params'),
     url(r'^$', views.index, name='index')
 )
--- mud/objects/views.py
+++ mud/objects/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404
 
 from objects.models import *
+from objects.forms import *
 
 def index(request):
     objs = Object.objects
@@ -42,3 +43,37 @@ def shop(request):
         data = ''
     context = {'objs': objs, 'data': data, }
     return render(request, 'objects/shop.html', context)
+
+def params(request):
+    f = ParamsForm()
+    if request.method == 'GET':
+        f = ParamsForm(request.GET)
+    f.is_valid()
+    
+    objs = Object.objects.all()
+    if f.cleaned_data['name'] != '':
+        objs = objs.filter(name__icontains = f.cleaned_data['name'])
+    if f.cleaned_data['wear']:
+        objs = objs.filter(wear = f.cleaned_data['wear'])
+    if f.cleaned_data['take']:
+        objs = objs.filter(take = f.cleaned_data['take'])
+    if f.cleaned_data['type']:
+        objs = objs.filter(type = f.cleaned_data['type'])
+    if f.cleaned_data['material']:
+        objs = objs.filter(material = f.cleaned_data['material'])
+    if f.cleaned_data['no_avail']:
+        objs = objs.filter(no_use = f.cleaned_data['no_use'])
+    if f.cleaned_data['no_avail']:
+        objs = objs.filter(no_avail = f.cleaned_data['no_avail'])
+    if f.cleaned_data['extra']:
+        objs = objs.filter(extra = f.cleaned_data['extra'])
+    if f.cleaned_data['affects']:
+        objs = objs.filter(affects = f.cleaned_data['affects'])
+    if f.cleaned_data['prop']:
+        objs = objs.filter(prop = f.cleaned_data['prop'])
+    
+    c = objs.count()
+    objs = objs[:30]
+    context = {'form': f, 'objs': objs, 'count': c,}
+    return render(request, 'objects/params.html', context)
+    
