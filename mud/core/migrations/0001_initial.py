9449be6a76365404cb30670302e33f20165bd40b
Nikolay Khokhlov <kolya.khokhlov@gmail.com>
2013-05-14 14:29:00 +0400

    Added south

---
+++ mud/core/migrations/0001_initial.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'User'
+        db.create_table(u'core_user', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'core', ['User'])
+
+        # Adding model 'Counter'
+        db.create_table(u'core_counter', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.TextField')()),
+            ('body', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'core', ['Counter'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'User'
+        db.delete_table(u'core_user')
+
+        # Deleting model 'Counter'
+        db.delete_table(u'core_counter')
+
+
+    models = {
+        u'core.counter': {
+            'Meta': {'object_name': 'Counter'},
+            'body': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {})
+        },
+        u'core.user': {
+            'Meta': {'object_name': 'User'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.TextField', [], {})
+        }
+    }
+
+    complete_apps = ['core']
