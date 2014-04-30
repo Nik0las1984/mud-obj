09093de4b985fd20879529e36113f03eed273e5e
Nikolay Khokhlov <kolya.khokhlov@gmail.com>
2014-04-30 13:00:45 +0400

    Статистика по дружинам

--- mud/squad/views.py
+++ mud/squad/views.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 
 import re
+import datetime
+import time
 
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -16,5 +18,49 @@ def index(request):
 def by_name(request, name):
     f = Squad.objects.filter(short_name__iexact = name)
     if f.count() > 0:
-        return render(request, 'squad/squad.html', { 'squad': f[0], })
+        sq = f[0]
+        d = datetime.datetime.now() - datetime.timedelta(days = 30)
+        info = sq.info.filter(date__gt = d).order_by("date")
+        xdata = []
+        exp_30 = []
+        exp_total = []
+        population = []
+        for i in info:
+            xdata.append(time.mktime(i.date.timetuple()))
+            exp_30.append(i.exp_30 / 1000)
+            exp_total.append(i.exp_total / 1000)
+            population.append(i.population)
+        
+        extra = {"tooltip": {"y_start": "", "y_end": ""},
+                   "date_format": "%d %b",}
+        
+        cd_exp_30 = {
+            'x': xdata,
+            'name1': u'Опыта за 30 дней (тыс)', 'y1': exp_30, 'extra1': extra,}
+        
+        cd_exp_total = {
+            'x': xdata,
+            'name1': u'Всего опыта (тыс)', 'y1': exp_total, 'extra1': extra,}
+        
+        cd_population = {
+            'x': xdata,
+            'name1': u'Людей', 'y1': population, 'extra1': extra,}
+        
+        charttype = "stackedAreaChart"
+        
+        req = {
+            'charttype': charttype,
+            'cd_exp_30': cd_exp_30,
+            'cd_exp_total': cd_exp_total,
+            'cd_population': cd_population,
+            'squad': sq,
+            'extra': {
+            'x_is_date': True,
+            'x_axis_format': "%d %b",
+            'tag_script_js': True,
+            'jquery_on_ready': True,
+            
+        },
+    }
+        return render(request, 'squad/squad.html', req)
     raise Http404
