264a7960326c9bdc459e53b102fb31543dd6765a
Nikolay Khokhlov <kolya.khokhlov@gmail.com>
2016-11-01 10:34:43 +0300

    Fix settings

--- mud/mud/settings.py
+++ mud/mud/settings.py
@@ -55,12 +55,12 @@ USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -232,6 +232,8 @@ WIKI_MARKDOWN_KWARGS = {
         'sane_lists',
     ],
 }
+    
+WIKI_IMAGES_PATH = os.path.join(BASE_DIR, 'media/images/%aid/')
 
 
 try:
